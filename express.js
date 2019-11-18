f3d557328bee0bbacfec43d07d362605a6eb7436
fanticus <russia_09091997@mail.ru>
2019-11-19 00:08:56 +0400

    Edit responses to customer

--- express.js
+++ express.js
@@ -1,8 +1,11 @@
-const http = require('http');
+const express = require('express');
 const ws = require('ws');
 const fs = require("fs");
 const port = 3000;
-const wss = new ws.Server({noServer: true});
+const app = express();
+const wss = new ws.Server({
+  noServer: true
+});
 
 fs.readFile('server-state.json', (error, data) => {
   console.log('Асинхронное чтение файла');
@@ -11,9 +14,8 @@ fs.readFile('server-state.json', (error, data) => {
     throw error; // если возникла ошибка
   }
   if (data.byteLength) {
-    // console.log('Данные: ', data.byteLength);
-    data = JSON.parse(data);
-    gameRooms = data;
+      // console.log('Данные: ', data.byteLength);
+    gameRooms = JSON.parse(data);
     gameRooms.forEach(game => {
       game.timer = setTimer(game);
     })
@@ -28,29 +30,31 @@ const requestHandler = (request, response) => {
   }
 
   wss.handleUpgrade(request, request.socket, Buffer.alloc(0), onConnect);
-}
+};
 
 const onConnect = (socket) => {
 
-  socket.on('close', function() {
-    let player;
-    const state = 'CLOSE';
-    let game = gameRooms.find(game => {
-      player = game.players.find(player => player.socket === socket);
-      return player;
-    })
+    socket.on('close', function() {
 
-    if (game && game.players.length === 2) {
-      player.socket = null;
-      game.messages.unshift(player.username + ' disconnected');
-      const {player1, player2} = getPlayers(game);
-      sendState(game, state, player1, player2);
-    } else if (game){
-      deleteGame(game);
-    }
-  });
+        let player;
+        const state = 'CLOSE';
+        let game = gameRooms.find(game => {
+            player = game.players.find(player => player.socket === socket);
+            return player;
+        });
+
+        if (game && game.players.length === 2) {
+            player.socket = null;
+            game.messages.unshift(player.username + ' disconnected');
+            const { player1, player2 } = getPlayers(game);
+            sendState(game, state, player1, player2);
+        } else if (game) {
+            deleteGame(game);
+        }
+    });
 
   socket.on('message', function (gameSettings) {
+
     gameSettings = JSON.parse(gameSettings);
     let { state, username, userID } = gameSettings;
 
@@ -72,14 +76,17 @@ const onConnect = (socket) => {
         // событие, когда вышло время
         // break;
       default:
-        socket.send(JSON.stringify({message: 'Данное событие на сервере не найдено', state}))
+        socket.send(JSON.stringify({
+          message: 'Данное событие на сервере не найдено',
+          state
+        }));
         break;
     }
   });
-}
+};
 
 
-const server = http.createServer(requestHandler)
+const server = app.use(requestHandler);
 server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
@@ -91,22 +98,21 @@ server.listen(port, (err) => {
 
 // -----------------------
 
-
 // вводим переменную timer в конструкторе игры
 // каждый раз, когда отправляем стейт игры игрокам, обновляем таймер (функция sendState)
 function setTimer(game) {
   let delay = 18e4;                     // таймер устанавливаем на 3 минуты
   let callback;
   console.log('запускаем таймер');
-  const state = 'TIMEOUT'
+  const state = 'TIMEOUT';
 
   if (game.gameOn) {
     callback = () => {
       game.winner = game.players.find(player => !player.playerIsShooter);
-      game.gameOver = true
+      game.gameOver = true;
       game.messages.unshift('** Game over **', '-', '-', game.winner + ' is winner', '-');
 
-      const {player1, player2} = getPlayers(game);
+      const { player1, player2 } = getPlayers(game);
       console.log('время вышло');
 
       sendState(game, state, player1, player2)
@@ -114,14 +120,14 @@ function setTimer(game) {
   } else {
     callback = () => {
       game.winner = game.players.find(player => player.isReady);
-      game.gameOver = true
+      game.gameOver = true;
       if (game.winner) {
         game.messages.unshift('** Game over **', '-', '-', game.winner + ' is winner', 'Time\'s up', '-');
       } else {
         game.messages.unshift('** Game over **', '-', '-', 'There is no winner', '-');
       }
 
-      const {player1, player2} = getPlayers(game);
+      const { player1, player2 } = getPlayers(game);
       console.log('время вышло');
 
       sendState(game, state, player1, player2)
@@ -135,41 +141,46 @@ function setTimer(game) {
 function pass(userID, state) {
   let game = checkGame(userID);
   if (game.players.length === 2) {
-    player = game.players.find(player => player.id === userID);
+    const player = game.players.find(player => player.id === userID);
     game.winner = game.players.find(player => player.id !== userID).username;
-    game.gameOver = true
+    game.gameOver = true;
     game.messages.unshift('** Game over **', '-', '-', game.winner + ' is winner', player.username + ' give up', '-');
 
-    const {player1, player2} = getPlayers(game);
+    const { player1, player2 } = getPlayers(game);
     sendState(game, state, player1, player2);
   }
 }
-
 
 function fire(userID, gameSettings, state) {
   let game = checkGame(userID);
   let player = game.players.find(player => player.id === userID);
   let enemy = game.players.find(player => player.id !== userID);
-  const { firedCell } = gameSettings;
-  firedCell = enemy.field[firedCell.coordX][firedCell.coordY]
-  const { cellStatus, isShip, idShip } = firedCell;
+  let message = null;
+  const {
+      cell: {
+        coordX,
+        coordY
+      }
+  } = gameSettings;
+  const cell = enemy.field[coordX][coordY];
+  const { cellStatus, isShip, idShip } = cell;
 
   if (!cellStatus) {
     if (isShip) {
-      firedCell.cellStatus = 'hit';
+      cell.cellStatus = 'hit';
       const ship = enemy.ships.find((ship) => ship.id === idShip);
       ship.hits++;
 
       if (ship.hits === ship.size) {
         ship.isSunk = true;
-        message = `${player.username} sank a ${enemy.username}'s ship on x: ${firedCell.coordY + 1} y: ${firedCell.coordX + 1}`;
+        message = `${player.username} sank a ${enemy.username}'s ship on x: ${coordY + 1} y: ${coordX + 1}`;
         ship.coords.forEach((coords) => setMissCellStatusAround(coords, enemy));
       } else {
-        message = `${player.username} shot ${enemy.username} on x: ${firedCell.coordY + 1} y: ${firedCell.coordX + 1}`;
+        message = `${player.username} shot ${enemy.username} on x: ${coordY + 1} y: ${coordX + 1}`;
       }
     } else {
-      firedCell.cellStatus = 'miss';
-      message = `${player.username} missed ${enemy.username} on x: ${firedCell.coordY + 1} y: ${firedCell.coordX + 1}`;
+      cell.cellStatus = 'miss';
+      message = `${player.username} missed ${enemy.username} on x: ${coordY + 1} y: ${coordX + 1}`;
       player.playerIsShooter = !player.playerIsShooter;
       enemy.playerIsShooter = !enemy.playerIsShooter;
     }
@@ -182,20 +193,21 @@ function fire(userID, gameSettings, state) {
     game.gameOver = true;
   }
 
-  const {player1, player2} = getPlayers(game);
-  sendState(game, state, player1, player2);
-}
+  const { player1, player2 } = getPlayers(game);
 
+  sendState(game, state, player1, player2);
+
+}
 
 function setMissCellStatusAround(coords, target) {
   const { coordX, coordY } = coords;
   const changeCellStatus = (target, coordX, coordY) => {
-    const cell = target.field[coordX][coordY];
     const isCell = target.field[coordX]
       && target.field[coordX][coordY];
 
-    if (isCell && !cell.isShip) {
-      cell.cellStatus = 'miss';
+    if (isCell) {
+      const cell = target.field[coordX][coordY];
+      if (!cell.isShip) cell.cellStatus = 'miss';
     }
   };
 
@@ -210,6 +222,7 @@ function setMissCellStatusAround(coords, target) {
 }
 
 
+
 function startGame(userID, gameSettings, state) {
   const { ships, field } = gameSettings;
 
@@ -220,19 +233,17 @@ function startGame(userID, gameSettings, state) {
   currentPlayer.ships = ships;
   currentPlayer.field = field;
 
-  shooterIndex = Math.round(Math.random() * (game.players.length - 1));
-  game.players[shooterIndex].playerIsShooter = true;
-
-  if (game.players.every(player => player.isReady)) {
+  if (game.players.length === 2 && game.players.every(player => player.isReady)) {
+    const shooterIndex = Math.round(Math.random() * (game.players.length - 1));
+    game.players[shooterIndex].playerIsShooter = true;
+    game.players[Number(!shooterIndex)].playerIsShooter = false;
     game.gameOn = true;
 
     const player1 = {
-      playerIsShooter: game.players[0].playerIsShooter,
-      isEnemyReady: true
+      playerIsShooter: game.players[0].playerIsShooter
     };
     const player2 = {
-      playerIsShooter: game.players[1].playerIsShooter,
-      isEnemyReady: true
+      playerIsShooter: game.players[1].playerIsShooter
     };
     sendState(game, state, player1, player2);
   }
@@ -246,28 +257,26 @@ function findGame(userID, username, socket, state) {
     console.log('игра найдена:');
 
     // <- тут надо переделать немного
-
   } else if (gameRooms.length && gameRooms[gameRooms.length - 1].players.length < 2) {
-    game = gameRooms[gameRooms.length - 1]
-    console.log('добавляем игрока')
+    game = gameRooms[gameRooms.length - 1];
+    console.log('добавляем игрока');
     game.players.push(new Player(userID, username))
   } else {
-    console.log('создаем новую игру')
+    console.log('создаем новую игру');
     game = createNewGame(userID, username)
   }
 
   let player = game.players.find(player => player.id === userID);
   if (!player.socket) {
-    game.messages.unshift(player.username + ' join the game');
+      game.messages.unshift(player.username + ' join the game');
   }
   player.socket = socket;
 
   if (game.players.length === 2) {
-    const {player1, player2} = getPlayers(game);
-    sendState(game, state, player1, player2);
+      const { player1, player2 } = getPlayers(game);
+      sendState(game, state, player1, player2);
   }
 }
-
 
 function deleteGame(game) {
   let gameIndex = gameRooms.findIndex(currentGame => game.gameID === currentGame.gameID);
@@ -276,13 +285,12 @@ function deleteGame(game) {
 
 
 function sendState(game, state, player1, player2) {
-  clearTimeout(game.timer);
-
+  clearTimeout(game.timer)
   if (game.players[0].socket) {
-    game.players[0].socket.send(JSON.stringify({state, ...player1}))
+    game.players[0].socket.send(JSON.stringify({ state, ...player1 }))
   }
   if (game.players[1].socket) {
-    game.players[1].socket.send(JSON.stringify({state, ...player2}))
+    game.players[1].socket.send(JSON.stringify({ state, ...player2 }))
   }
 
   if (game.gameOver) {
@@ -292,12 +300,12 @@ function sendState(game, state, player1, player2) {
   }
 
   const jsonReplacer = (key, value) => {
-    if (key == 'timer' || key == 'socket') {
+    if (key === 'timer' || key === 'socket') {
       return undefined;
     }
     return value;
-  }
-  data = JSON.stringify(gameRooms, jsonReplacer)
+  };
+  const data = JSON.stringify(gameRooms, jsonReplacer);
   fs.writeFile('server-state.json', data, (error) => {
     console.log('Асинхронная запись файла');
     if (error) {
@@ -307,8 +315,7 @@ function sendState(game, state, player1, player2) {
   });
 }
 
-
-function getPlayers (game) {
+function getPlayers(game) {
   const { gameOn, gameOver, winner, messages } = game;
   const player1 = {
     gameOn,
@@ -325,9 +332,9 @@ function getPlayers (game) {
     },
     enemy: {
       username: game.players[1].username,
-      field: game.players[1].field,
+      field: game.players[1].field
     }
-  }
+  };
   const player2 = {
     gameOn,
     gameOver,
@@ -343,11 +350,11 @@ function getPlayers (game) {
     },
     enemy: {
       username: game.players[0].username,
-      field: game.players[0].field,
+      field: game.players[0].field
     }
-  }
+  };
 
-  return {player1, player2}
+  return { player1, player2 }
 }
 
 
@@ -357,6 +364,7 @@ function createNewGame(playerID, username) {
   gameRooms.push(newGame);
   return gameRooms[gameRooms.length - 1];
 }
+
 
 // конструктор для объекта "игра"
 function Game(id) {
@@ -377,7 +385,7 @@ function Player(id, username) {
   this.playerIsShooter = false;
   this.socket = null;
   this.isReady = false;
-};
+}
 
 // массив игровых комнат
 let gameRooms = [];
